8fd2a3144c2529f5cb5eec517565c0c4d89bd92d
Mikkel Danielsen <md@evercall.dk>
2025-04-21 14:26:22 +0200

    Use correct method

--- src/OpenRouterRequest.php
+++ src/OpenRouterRequest.php
@@ -66,7 +66,7 @@ final class OpenRouterRequest extends OpenRouterAPI
             $options
         );
 
-		if(isset($response['error']['code']) && $response['error']['code'] === 429) {
+		if($response->getStatusCode() === 429) {
 			return new ErrorData(
 				code: 429,
 				message: 'Rate limit exceeded. Please try again later.',
